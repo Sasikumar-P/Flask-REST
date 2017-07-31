3dabbbd496a1f3e4c2fa956f67e3a1bd86e99130
Sasikumar <skp.guptha@gmail.com>
2017-07-31 11:50:35 +0530

    first commit

---
+++ nextflaskrest.py
@@ -0,0 +1,58 @@
+
+from flask import Flask, url_for,request,json,Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def api_root():
+    return 'Welcome'
+
+@app.route('/articles')
+def api_articles():
+    return 'List of ' + url_for('api_articles')
+
+@app.route('/articles/<articleid>')
+def api_article(articleid):
+    return 'You are reading ' + articleid
+
+#@app.route('/hello')
+#def api_hello():
+#    if 'name' in request.args:
+#       return 'Hello ' + request.args['name']
+ #   else:
+  #      return 'Hello John Doe'
+
+
+@app.route('/hello', methods = ['GET'])
+def api_hello():
+    data = {
+        'hello'  : 'world',
+        'number' : 3
+    }
+    js = json.dumps(data)
+
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://luisrei.com'
+
+    return resp
+
+@app.route('/messages', methods = ['POST'])
+def api_message():
+    if request.headers['Content-Type'] == 'text/plain':
+        return "Text Message: " + request.data
+
+    elif request.headers['Content-Type'] == 'application/json':
+        return "JSON Message: " + json.dumps(request.json)
+
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return "Binary message written!"
+
+    else:
+        return "415 Unsupported Media Type ;)"
+
+if __name__ == '__main__':
+    app.run()
+
